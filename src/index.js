e332c1f4f6ac6717967ee97d01328851224ac6f2
Robinson Lam <robinsonlam94@gmail.com>
2018-04-13 13:43:55 +1000

    exact path on /

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './index.scss';
 
@@ -13,7 +13,7 @@ const destination = document.getElementById('root');
 ReactDOM.render(
   <BrowserRouter>
     <App>
-      <Route path="/" component={Landing}/>
+      <Route exact path="/" component={Landing}/>
       <Route path="/myteams" component={MyTeams}/>
       <Route path="/login" component={Landing}/>
     </App>
